0035679bc6e53454631df42a6a52974b8c71d086
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-27 14:40:22 +0100

    create migration table trains

---
+++ database/migrations/2024_02_27_125320_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency',64);
+            $table->string('departure_station',96);
+            $table->string('arrival_station',96);
+            $table->time('departure_time', $precision = 0)->nullable();
+            $table->time('arrival_time', $precision = 0)->nullable();
+            $table->smallInteger('train_code')->unique()->unsigned();
+            $table->tinyInteger('number_carriages')->nullable()->unsigned();
+            $table->boolean('timetable')->default(true);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
