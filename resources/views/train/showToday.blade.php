1eafab9f75e61659c50f0f9ca6c7bd160459a84b
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-27 18:06:41 +0100

    add some style

--- resources/views/train/showToday.blade.php
+++ resources/views/train/showToday.blade.php
@@ -7,20 +7,57 @@
     Treni in partenza oggi 
 </h1>
 <h2 class="text-center">
-        {{ $date }}
+    {{ $date }}
 </h2>
-@foreach ($trains as $train)
-<div>
-         
-    {{ "Azienda: ".$train['agency'] }}
-    {{ $train['departure_station'] }}
-    {{ $train['arrival_station'] }}
-    {{ $train['departure_time'] }}
-    {{ $train['arrival_time'] }}
-    {{ $train['train_code'] }}
-    {{ $train['timetable'] }}
-    {{ $train['deleted'] }}
 
+<div class="container">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Azienda</th>
+                <th scope="col">Partenza</th>
+                <th scope="col">Arrivo</th>
+                <th scope="col">Orario Partenza</th>
+                <th scope="col">Orario Arrivo</th>
+                <th scope="col">Codice treno</th>
+                <th scope="col">Note</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td>
+                        {{ "Azienda: ".$train['agency'] }}
+                    </td>
+                    <td>
+                        {{ $train['departure_station'] }}
+                    </td>
+                    <td>
+                        {{ $train['arrival_station'] }}
+                    </td>
+                    <td>
+                        {{ $train['departure_time'] }}
+                    </td>
+                    <td>
+                        {{ $train['arrival_time'] }}
+                    </td>
+                    <td>
+                        {{ $train['train_code'] }}
+                    </td>
+                    <td>
+                        @if ($train['timetable']==1)
+                        Treno in orario
+                        @else
+                        Treno in ritardo
+                        @endif
+                        @if ($train['deleted']==1)
+                        Cancellato
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+        
 </div>
-@endforeach
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,23 +15,59 @@ $dateIta = $dateNow->format('Y-m-d');
     Treni
 </h1>
 <h2 class="text-center">
-    <a href="{{ route('train.show', ['date' => $dateIta])}}" class=" nav-underline ">
-        {{ $dateIta }}
+    <a href="{{ route('train.show', ['date' => $dateIta])}}" class="text-decoration-none">
+        Vedi i treni che partono in data:{{ $dateIta }}
     </a>
 </h2>
-@foreach ($trains as $train)
-<div>
-         
-    {{ "Azienda: ".$train['agency'] }}
-    {{ $train['departure_station'] }}
-    {{ $train['arrival_station'] }}
-    {{ $train['departure_time'] }}
-    {{ $train['arrival_time'] }}
-    {{ $train['train_code'] }}
-    {{ $train['timetable'] }}
-    {{ $train['deleted'] }}
-
+<div class="container">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Azienda</th>
+                <th scope="col">Partenza</th>
+                <th scope="col">Arrivo</th>
+                <th scope="col">Orario Partenza</th>
+                <th scope="col">Orario Arrivo</th>
+                <th scope="col">Codice treno</th>
+                <th scope="col">Note</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td>
+                        {{ "Azienda: ".$train['agency'] }}
+                    </td>
+                    <td>
+                        {{ $train['departure_station'] }}
+                    </td>
+                    <td>
+                        {{ $train['arrival_station'] }}
+                    </td>
+                    <td>
+                        {{ $train['departure_time'] }}
+                    </td>
+                    <td>
+                        {{ $train['arrival_time'] }}
+                    </td>
+                    <td>
+                        {{ $train['train_code'] }}
+                    </td>
+                    <td>
+                        @if ($train['timetable']==1)
+                        Treno in orario
+                        @else
+                        Treno in ritardo
+                        @endif
+                        @if ($train['deleted']==1)
+                        Cancellato
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+        
 </div>
-@endforeach
 
 @endsection
